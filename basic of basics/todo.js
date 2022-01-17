1db00c1ef5e73f5f49977fbc295ee825d79b887b
suhasBhat2002 <suhas.r.bhat123@gmail.com>
2022-01-17 22:57:50 +0530

    third commit

--- basic of basics/todo.js
+++ basic of basics/todo.js
@@ -1,5 +1,33 @@
-alert('connected')
-var h = document.createElement("h1")
-var myValue = document.createTextNode("hello people")
-h.appendChild(myValue)
-document.querySelector('h1').appendChild(h)
+// alert('connected')
+// var h = document.createElement("h1")
+// var myValue = document.createTextNode("hello people")
+// h.appendChild(myValue)
+// document.querySelector('h1').appendChild(h)
+
+
+var ul = document.getElementById('list')
+var li;
+
+var addButton = document.getElementById('add')
+addButton.addEventListener('add',addItem)
+
+var removeItem = document.getElementById('remove')
+removeItem.addEventListener('click',removeItem)
+
+
+
+function addItem(){
+    console.log('add button clicked')
+}
+
+
+
+
+function removeItem(){
+for (let index = 0; index < li.length; index++) {
+    while (li[index] && li[index].children[0].checked) {
+        ul.removeChild(li[index])
+    }
+    
+}
+}
