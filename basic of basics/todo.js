84bea45a86664ac93a8b6e4500b328b09a9b807f
suhasBhat2002 <suhas.r.bhat123@gmail.com>
2022-01-18 22:40:09 +0530

    fourth commit

--- basic of basics/todo.js
+++ basic of basics/todo.js
@@ -24,6 +24,7 @@ function addItem(){
 
 
 function removeItem(){
+    li = ul.children
 for (let index = 0; index < li.length; index++) {
     while (li[index] && li[index].children[0].checked) {
         ul.removeChild(li[index])
