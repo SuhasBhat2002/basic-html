414730111f2bd9f9e60460e4f68ed49b9f34918f
suhasBhat2002 <suhas.r.bhat123@gmail.com>
2022-01-21 22:19:01 +0530

    seventh commit

---
+++ practice/localStorage.js
@@ -0,0 +1,6 @@
+localStorage.setItem('Army','100');
+localStorage.setItem('Generals','3');
+var myLegion = localStorage.getItem('Army')
+console.log(myLegion)
+
+console.log(localStorage.getItem('Generals'));
