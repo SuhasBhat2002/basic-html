f44a580456b57dd9edca7c336982306526b3d8ff
suhasBhat2002 <suhas.r.bhat123@gmail.com>
2022-01-31 22:31:22 +0530

    16th commit

--- practice/localStorage.js
+++ practice/localStorage.js
@@ -4,3 +4,24 @@ var myLegion = localStorage.getItem('Army')
 console.log(myLegion)
 
 console.log(localStorage.getItem('Generals'));
+
+let mybutton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function King(){
+    let king_title = prompt('The sorcerer supreme');
+    localStorage.setItem('title',king_title);
+    myHeading.textContent = 'suhas' + king_title;
+}
+
+if(!localStorage.getItem('title')) {
+    setUserName();
+  } else {
+    let storedName = localStorage.getItem('title');
+    myHeading.textContent = 'suhas, ' + storedName;
+  }
+  myButton.onclick = King(){
+  
+    setUserName();
+  }
+  
