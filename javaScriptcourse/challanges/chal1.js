842e68b0b588d47694cf8f761c8ea5757b11b28f
suhasBhat2002 <suhas.r.bhat123@gmail.com>
2022-02-08 20:39:59 +0530

    23th commit

---
+++ javaScriptcourse/challanges/chal1.js
@@ -0,0 +1,17 @@
+var mark = {
+    name : 'my name is mark',
+    mass : 85,
+    height : 6,
+}
+
+var sam ={
+    name : 'my name is sam',
+    mass : 70,
+    height :5,
+}
+
+var bmi = function(name){
+    console.log(mark.mass/mark.height**2)
+}
+
+console.log(bmi)
