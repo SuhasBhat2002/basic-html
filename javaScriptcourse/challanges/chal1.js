3c37ec1a566b3e940102ebedf5232bb1a97daf57
suhasBhat2002 <suhas.r.bhat123@gmail.com>
2022-02-10 20:47:27 +0530

    26th commit

--- javaScriptcourse/challanges/chal1.js
+++ javaScriptcourse/challanges/chal1.js
@@ -8,3 +8,9 @@ const BMIjohn = johnMass/johnHeight**2;
 const whoseHigher = BMImark>BMIjohn;
 
 console.log(BMImark,BMIjohn,whoseHigher);
+if (BMImark<BMIjohn) {
+    console.log(`BMI of john(${BMIjohn}) is greater than marks(${BMImark})`)
+} 
+if (BMImark>BMIjohn) {
+    console.log(`BMI of mark(${BMImark}) is greater than johns(${BMIjohn})`)
+}
