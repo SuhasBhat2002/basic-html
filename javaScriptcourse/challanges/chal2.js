9bea06b438f06bb3ce822bd1ec83ab505d34c889
suhasBhat2002 <suhas.r.bhat123@gmail.com>
2022-02-13 22:12:48 +0530

    29th commit

---
+++ javaScriptcourse/challanges/chal2.js
@@ -0,0 +1,11 @@
+const dolphine = 98 + 108 + 89;
+const averageD = dolphine/3;
+
+const koalas = 88 + 91 + 110;
+const averageK = koalas/3;
+
+if (averageD<averageK) {
+    console.log('winner is dolphine')
+}else{
+    console.log('koalas is the winner')
+}
