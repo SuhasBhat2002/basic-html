eab35fce95c19d62d27c254bb3c92e7bfdafc3b1
suhasBhat2002 <suhas.r.bhat123@gmail.com>
2022-02-17 22:24:52 +0530

    32th commit

--- javaScriptcourse/challanges/chal2.js
+++ javaScriptcourse/challanges/chal2.js
@@ -1,12 +1,12 @@
-const dolphine = 98 + 108 + 89;
+const dolphine = 97 + 112 + 101;
 const averageD = dolphine/3;
 
-const koalas = 88 + 91 + 110;
+const koalas = 109 + 95 + 106;
 const averageK = koalas/3;
 
-if (averageD<averageK) {
+if (averageD > averageK && averageD >= 100) {
     console.log('winner is dolphine');
-}else if(averageD>averageK){
+}else if(averageD < averageK && averageK >= 100){
     console.log('koalas is the winner');
 }else if(averageD===averageK){
 console.log('the match is a draw');
