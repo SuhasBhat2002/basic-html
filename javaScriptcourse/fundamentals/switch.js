4cc52cc32dd8eaa6f6024b6049b2caed592e1396
suhasBhat2002 <suhas.r.bhat123@gmail.com>
2022-02-18 22:13:40 +0530

    32th commit

---
+++ javaScriptcourse/fundamentals/switch.js
@@ -0,0 +1,15 @@
+const day = 'monday'
+
+switch(day){
+    case 'monday':
+        console.log("sleep well today");
+        break;
+    case 'tuesday':
+        console.log("sleep well today too");
+        break;
+        case 'wednesday':
+        console.log("just give up on life");
+        break;
+        default:
+        console.log("you are already dead in pubg,lol")
+}
