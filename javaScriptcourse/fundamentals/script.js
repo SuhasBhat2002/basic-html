9db798cd912c452edbc792c7339abe9941240d08
suhasBhat2002 <suhas.r.bhat123@gmail.com>
2022-02-24 20:59:16 +0530

    36th commit

---
+++ javaScriptcourse/fundamentals/script.js
@@ -0,0 +1,5 @@
+'use strict';
+//it resticts some fucture reserve words and helps avoid mistakes
+//const interface = 'audio'
+//const private = 'damn' doesnt work as java script has reserved it to make future
+//logic
