e817e33ca19f75572badaebea5ae3ad20c1a7841
suhasBhat2002 <suhas.r.bhat123@gmail.com>
2022-02-26 23:08:30 +0530

    37th commit

--- javaScriptcourse/fundamentals/script.js
+++ javaScriptcourse/fundamentals/script.js
@@ -4,3 +4,11 @@
 //const private = 'damn' doesnt work as java script has reserved it to make future
 //logic
 //const if = csoivns doesnt work because it is used in 'if' statement
+
+function fruitProcessor(appels,oranges){
+    const juice = `the juice is made up of ${appels} appels and ${oranges} oranges`
+return juice;
+}
+console.log(fruitProcessor(10,6));
+const drink = fruitProcessor(11,8);
+console.log(drink);
