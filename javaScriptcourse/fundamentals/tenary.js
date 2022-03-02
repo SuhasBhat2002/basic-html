126de838bf0dc67b6af2bbbf89836ad46aa07283
suhasBhat2002 <suhas.r.bhat123@gmail.com>
2022-03-02 23:24:19 +0530

    41st commit

--- javaScriptcourse/fundamentals/tenary.js
+++ javaScriptcourse/fundamentals/tenary.js
@@ -4,3 +4,4 @@ const drink = age>=18 ? 'wine' : 'juice';
 console.log(drink);
 
 console.log(`i like to drink ${age>=18 ? 'wine' : 'juice'}`);
+console.log(`i like to drink ${age<=18 ? 'wine' : 'juice'}`);
