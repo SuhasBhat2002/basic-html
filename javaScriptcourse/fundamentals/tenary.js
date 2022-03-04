da53cd7150431e250203201da1ee076ebbf54bc5
SuhasBhat2002 <96760562+SuhasBhat2002@users.noreply.github.com>
2022-03-05 05:28:13 +0530

    Update tenary.js

--- javaScriptcourse/fundamentals/tenary.js
+++ javaScriptcourse/fundamentals/tenary.js
@@ -6,3 +6,5 @@ console.log(drink);
 console.log(`i like to drink ${age>=18 ? 'wine' : 'juice'}`);
 console.log(`i like to drink ${age<=18 ? 'wine' : 'juice'}`);
 console.log(`i like to eat ${age>=18 ? 'gobi' : 'panner'}`);
+//tenary makes  the code short and neat
+
