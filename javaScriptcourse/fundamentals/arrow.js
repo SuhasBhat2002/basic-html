0c7a35eab4521c411d6d1ef1e28941fb6b4cb94e
suhasBhat2002 <suhas.r.bhat123@gmail.com>
2022-03-08 22:53:20 +0530

    44th commit

--- javaScriptcourse/fundamentals/arrow.js
+++ javaScriptcourse/fundamentals/arrow.js
@@ -5,3 +5,10 @@ const tillRetirement = (birthyear,firstname) => {
     
 }
 console.log(tillRetirement(1987,'john'))
+
+const tillRetirement2 = function (birthyear,firstname) {
+    const age = 2037 - birthyear;
+    const retairment = 65 - age;
+    return `${firstname} retires in ${retairment} years`
+    
+}
