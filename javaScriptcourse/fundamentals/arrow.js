384b862c3cf333647ad9f26973ac149bd19c5d69
suhasBhat2002 <suhas.r.bhat123@gmail.com>
2022-03-09 23:10:42 +0530

    45th

--- javaScriptcourse/fundamentals/arrow.js
+++ javaScriptcourse/fundamentals/arrow.js
@@ -6,9 +6,15 @@ const tillRetirement = (birthyear,firstname) => {
 }
 console.log(tillRetirement(1987,'john'))
 
+const calcage5 = function(birthyear){
+    return 2037 - birthyear
+}
+
 const tillRetirement2 = function (birthyear,firstname) {
-    const age = 2037 - birthyear;
+    const age = calcage5(birthyear);
     const retairment = 65 - age;
     return `${firstname} retires in ${retairment} years`
     
 }
+
+console.log(tillRetirement2(1991,'derrick'))
