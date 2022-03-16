ec92501244f87a22e3e0488a5e85b544f4a14a0e
suhasBhat2002 <suhas.r.bhat123@gmail.com>
2022-03-16 22:01:07 +0530

    45th

--- javaScriptcourse/challanges/chal4.js
+++ javaScriptcourse/challanges/chal4.js
@@ -1,4 +1,8 @@
-const calcAverage = (score1,score2,score3) => {
-    return (score1 + score2 + score3)*3;
+//strict code
+function calcAverage(score1, score2, score3) {
+    return (score1 + score2 + score3) * 3;
 }
-avgDholpine = h
+const dolphineAvg = function(score1,score2,score3){
+    const Davg = calcAverage(score1,score2,score3)
+}
+console.log(dolphineAvg(44,23,71));
