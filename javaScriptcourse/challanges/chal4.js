1f1cf31443dcf719981921ee9491c36c6ae9698a
suhasBhat2002 <suhas.r.bhat123@gmail.com>
2022-03-24 22:57:31 +0530

    46th

--- javaScriptcourse/challanges/chal4.js
+++ javaScriptcourse/challanges/chal4.js
@@ -3,4 +3,4 @@ console.log(calcAverage(5,6,9))
 
 const scoreDolphins = calcAverage(44,23,71);
 const scoreKolas = calcAverage(65,54,49);
-console.log(calcAverage(55,33,88));
+console.log(scoreDolphins,scoreKolas);
