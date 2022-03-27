d03fefa8e67e1bbcad60632ba1017fb2427bedf2
suhasBhat2002 <suhas.r.bhat123@gmail.com>
2022-03-27 22:31:39 +0530

    48th

--- javaScriptcourse/challanges/chal4.js
+++ javaScriptcourse/challanges/chal4.js
@@ -4,4 +4,10 @@ console.log(calcAverage(5,6,9))
 const scoreDolphins = calcAverage(44,23,71);
 const scoreKolas = calcAverage(65,54,49);
 console.log(scoreDolphins,scoreKolas);
-console.log(scoreDolphins,scoreKolas);
+
+if(scoreDolphins>scoreKolas){
+    console.log("dolpins are the winner")
+}
+else{
+    console.log("kolas are the winner")
+}
