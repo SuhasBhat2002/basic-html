cbd7f861ed98c57453fe779f2af57d05e55adaee
SuhasBhat2002 <suhas.r.bhat123@gmail.com>
2022-04-08 21:36:31 +0530

    53

--- javaScriptcourse/challanges/chal5.js
+++ javaScriptcourse/challanges/chal5.js
@@ -15,3 +15,6 @@ const Tip = [calcTip(bills[0]),calcTip(bills[1]),calcTip(bills[2])];
 
 console.log(Tip);
 
+const total = [bills[0] + Tip[0],bills[1] + Tip[1],bills[2] + Tip[3]]
+
+console.log(bills,Tip,total);
